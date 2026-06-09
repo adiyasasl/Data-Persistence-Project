02f055c940ae76734501be7a2f04aaabe55e3ec6
Adiyasa Setia Lesmana <yasaaadi@gmail.com>
2026-06-09 12:57:59 +0700

    Initial Main

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(int buildIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(buildIndex);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
